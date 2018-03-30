f004fd98fe5b83b0efd7a6365adced5d805302b7
ProgrammerMuda <ragamuthahari@gmail.com>
2018-03-30 20:59:42 +0700

    Update-1

--- CRUD/app/src/main/java/id/rbs/raga/crud/function/MyConstant.java
+++
@@ -1,9 +0,0 @@
-package id.rbs.raga.crud.function;
-
-/**
- * Created by RAGA on 22/03/2018.
- */
-
-public class MyConstant {
-    public static final String BASE_URL = "http://192.168.2.61/db_inventori/";
-}
